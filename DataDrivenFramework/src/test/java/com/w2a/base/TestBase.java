ff67cf64933ea5c206028f9724c62738b96fa8a7
praful vanker <pmvanker@gmail.com>
2025-08-10 17:46:06 +0530

    setup for macbook m1

--- DataDrivenFramework/src/test/java/com/w2a/base/TestBase.java
+++ DataDrivenFramework/src/test/java/com/w2a/base/TestBase.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -38,35 +39,44 @@ public class TestBase {
 	@BeforeSuite	
 	public void setUp() throws IOException{
 		try {
-			config = new Properties();
-			OR = new Properties();
-			
-			fis = new FileInputStream( System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
-			log.debug("Loading Config file");
-			config.load(fis);
-			fis = new FileInputStream( System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
-			OR.load(fis);
-			if(config.getProperty("browser").equals("chrome"))
-			{
-				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\executable\\chromedriver.exe");
-				driver = new ChromeDriver();
-				log.debug("Chrome Browser driver");
-			}
-			if(config.getProperty("browser").equals("firefox"))
-			{
-				System.out.println("firefox driver");
-				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\executable\\geckodriver.exe");
-				driver = new FirefoxDriver();
-				log.debug("Firefox Browser driver");
-			}
-		} catch (FileNotFoundException e) {
+				config = new Properties();
+				OR = new Properties();
+				
+				fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/properties/Config.properties");
+				log.debug("Loading Config file");
+				config.load(fis);
+				fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/properties/OR.properties");
+				OR.load(fis);
+				if(config.getProperty("os").equals("mac"))
+				{
+					WebDriverManager.chromedriver().setup();
+				    driver = new ChromeDriver();
+
+				}
+				else
+				{
+					if(config.getProperty("browser").equals("chrome"))
+					{
+						System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\executable\\chromedriver.exe");
+						driver = new ChromeDriver();
+						log.debug("Chrome Browser driver");
+					}
+					if(config.getProperty("browser").equals("firefox"))
+					{
+						System.out.println("firefox driver");
+						System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\executable\\geckodriver.exe");
+						driver = new FirefoxDriver();
+						log.debug("Firefox Browser driver");
+					}
+				}
+			} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		System.out.println(config.getProperty("testurl"));
-		driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("ImplicityWait")), TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get(config.getProperty("testurl"));
+			}
+			System.out.println(config.getProperty("testurl"));
+//			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("ImplicityWait")), TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get(config.getProperty("testurl"));
 	}
 	
 	@AfterSuite
